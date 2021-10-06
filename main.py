ffd335ca7ba97df61203852b650490a462e3aca3
Arina <arina2004popova@yandex.ru>
2021-10-06 15:41:34 +0300

    Добавлены изменения

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import argparse
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+def is_bitlink(link):
+    splitted_link = urlparse(link)
+    without_scheme_link = '{}{}'.format(splitted_link.netloc, splitted_link.path)
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{without_scheme_link}'
+    response = requests.get(api_url, headers=headers)
+    return response.ok
+
+
+def shorten_link(headers, link):
+    request_body = {'long_url': link}
+    api_url = 'https://api-ssl.bitly.com/v4/shorten'
+    response = requests.post(api_url, headers=headers, json=request_body)
+    response.raise_for_status()
+    bitlink = response.json()['id']
+    return bitlink
+
+
+def count_clicks(headers, link):
+    splitted_link = urlparse(link)
+    without_scheme_link = '{}{}'.format(splitted_link.netloc, splitted_link.path)
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{without_scheme_link}/clicks/summary'
+    response = requests.get(api_url, headers=headers)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
+    return clicks_count
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    headers = {'Authorization': f'Bearer {os.getenv("BITLY_API_TOKEN")}'}
+    parser = argparse.ArgumentParser(
+        description='Сокращение ссылок'
+    )
+    parser.add_argument('link', help='Описание аргумента')
+    link = parser.parse_args().link
+    if is_bitlink(link):
+        try:
+            clicks_count = count_clicks(headers, link)
+            print(clicks_count)
+        except requests.exceptions.HTTPError:
+            exit('Ошибка в ссылке')
+    else:
+        try:
+            bitlink = shorten_link(headers, link)
+            print('Битлинк', bitlink)
+        except requests.exceptions.HTTPError:
+            exit('Ошибка в сокращенной ссылке')
